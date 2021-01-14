cd500b8f5c1872231850b1159e82a3ff4b003d4f
Chris Burns <ckbzxz@gmail.com>
2021-01-14 18:48:28 -0500

    added jawsdb connection in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,22 @@
 
 const mysql = require('mysql');
 const dotenv = require("dotenv");
+var connection;
 dotenv.config();
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: process.env.PASSWORD,
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: process.env.PASSWORD,
+        database: 'burgers_db'
+    });
+}
+
+
 
 connection.connect((err) => {
     if (err) throw err;
